24bbf9f250b82bbd610507e377647633ad98aa63
PrincessGod <jihongjiang@outlook.com>
2017-12-31 23:40:52 +0800

    fix build command in unix

--- rollup.config.js
+++ rollup.config.js
@@ -3,30 +3,33 @@ import eslint from 'rollup-plugin-eslint';
 import uglify from 'rollup-plugin-uglify';
 
 const output = [];
-if (process.env.NODE_ENV === 'module') {
-    output.push({
+console.log( process.env.NODE_ENV );
+if ( process.env.NODE_ENV === 'module' )
+    output.push( {
         format: 'es',
         sourcemap: true,
         file: 'build/czpg.module.js',
-    });
-} else {
-    output.push({
+    } );
+else
+    output.push( {
         format: 'umd',
         name: 'CZPG',
         sourcemap: true,
-        file: (process.env.NODE_ENV === 'combine') ? 'build/czpg.js' : 'build/czpg.min.js',
-    });
-}
+        file: ( process.env.NODE_ENV === 'combine' ) ? 'build/czpg.js' : 'build/czpg.min.js',
+    } );
 
-const plugins = [eslint()];
 
-if (process.env.NODE_ENV !== 'combine') {
-    plugins.push(babel({
+const plugins = [ eslint() ];
+
+if ( process.env.NODE_ENV !== 'combine' ) {
+
+    plugins.push( babel( {
         exclude: 'node_modules/**',
-    }));
-    if (process.env.NODE_ENV !== 'module') {
-        plugins.push(uglify());
-    }
+    } ) );
+    if ( process.env.NODE_ENV !== 'module' )
+        plugins.push( uglify() );
+
+
 }
 
 export default {
