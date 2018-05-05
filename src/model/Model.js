09174620778f9605fc3b7cf91786cb5a8cc7b20a
PrincessGod <jihongjiang@outlook.com>
2018-05-05 21:44:42 +0800

    fix typo

--- src/model/Model.js
+++ src/model/Model.js
@@ -345,7 +345,7 @@ Object.assign( Model.prototype, {
         if ( args[ 0 ] instanceof Transform )
             return this.setQuaternion( ...( args[ 0 ].quaternion.getArray() ) );
 
-        this.setQuaternion( ...args );
+        this.transform.setQuaternion( ...args );
         return this;
 
     },
