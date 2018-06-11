3181a1a100294196d533143c60ffa3c4532e7934
PrincessGod <jihongjiang@outlook.com>
2018-06-11 23:41:59 +0800

    add basic shader

---
+++ src/shader/shadersrc/basic.fs.glsl
@@ -0,0 +1,38 @@
+#include <common>
+#include <common_fs>
+#include <uv_spec_fs>
+#include <uv2_spec_fs>
+#include <normal_spec_fs>
+#include <color_spec_fs>
+#include <base_texture_spec_fs>
+#include <alpha_texture_spec_fs>
+#include <light_texture_spec_fs>
+#include <ao_texture_spec_fs>
+
+void main() {
+
+    #include <base_texture_fs>
+    #include <color_fs>
+    #include <alpha_texture_fs>
+    #include <alpha_mask_fs>
+    #include <alpha_blend_fs>
+
+    // specular map
+
+    ReflectedLight reflectedLight = ReflectedLight( vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ) );
+
+	#ifdef HAS_LIGHTTEXTURE
+
+		reflectedLight.indirectDiffuse += texture2D( u_lightTexture, vUv2 ).xyz * u_lightTextureIntensity;
+
+	#else
+
+		reflectedLight.indirectDiffuse += vec3( 1.0 );
+
+	#endif
+
+    reflectedLight.indirectDiffuse *= baseColor.rgb;
+	vec3 outgoingLight = reflectedLight.indirectDiffuse;
+    finalColor = vec4( outgoingLight, baseColor.a );
+
+}
---
+++ src/shader/shadersrc/basic.vs.glsl
@@ -0,0 +1,20 @@
+#include <common>
+#include <common_vs>
+#include <uv_spec_vs>
+#include <uv2_spec_vs>
+#include <normal_spec_vs>
+#include <color_spec_vs>
+
+void main() {
+
+    #include <begin_position_vs>
+    #include <begin_normal_vs>
+    #include <morph_vs>
+    #include <skinning_vs>
+    #include <uv_vs>
+    #include <uv2_vs>
+    #include <normal_vs>
+    #include <color_vs>
+    #include <position_vs>
+
+}
