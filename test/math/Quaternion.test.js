9697663590a9efd27ba6e4fad43d189391ba735f
PrincessGod <jihongjiang@outlook.com>
2018-08-27 21:44:49 +0800

    add quat test

---
+++ test/math/Quaternion.test.js
@@ -0,0 +1,47 @@
+import test from 'ava';
+import { Quaternion, Euler, Matrix4 } from '../../';
+
+const a = new Quaternion();
+const b = new Quaternion();
+
+test( '#constructor default w is 1', t => {
+
+    t.true( a.w === 1 );
+
+} );
+
+test( 'i#setFromEuler return default quaternion', t => {
+
+    a.setFromEuler( new Euler() );
+    t.true( a.equals( b.set( 0, 0, 0, 1 ) ) );
+
+} );
+
+test( 'i#setFromEuler circle test', t => {
+
+    const e = new Euler( Math.random(), Math.random(), Math.random() );
+    a.setFromEuler( e );
+    const o = new Euler().setFromQuaternion( a );
+    b.setFromEuler( o );
+    t.true( e.equals( o ) && a.equals( b ) );
+
+} );
+
+test( 'i#setFromMatrix4 return default quaternion', t => {
+
+    a.setFromMatrix4( new Matrix4() );
+    t.true( a.equals( b.set( 0, 0, 0, 1 ) ) );
+
+} );
+
+test( 'i#setFromMatrix4 circle test', t => {
+
+    const e = new Euler( Math.random(), Math.random(), Math.random() );
+    b.setFromEuler( e );
+    const m = new Matrix4().setFromQuat( b );
+    a.setFromMatrix4( m );
+    t.true( a.equals( b ) );
+
+} );
+
+test.todo( 'i#slerp do not how to test' );
